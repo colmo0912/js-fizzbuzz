da96e708a8a79939f9679a41739946dff5b14cd2
colmo0912 <colmo0912@gmail.com>
2021-04-12 15:36:48 +0200

    aggiungo gli if e else

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // scrivo i numeri da 1 a 100
 
-for (var i=0; i<=100; i++){
+
+
+var multiplo3 = i % 3 === 0;
+var multiplo5 = i % 5 === 0;
+
+
+
+// per ogni numero multiplo di 3 che il ciclo trova devo scrivere "Fizz"
+
+for(var i = 0; i <= 100; i++){
+    
+    if (i % 3 === 0){
+        console.log("Fizz")
+    }else if ( i % 5 === 0){
+        console.log("Buzz")
+}   else if ( i % 5 === 0 ||  i % 5 === 0){
+    console.log("FizzBuzz")
+}  else {
 
     console.log(i)
+    }
 }
